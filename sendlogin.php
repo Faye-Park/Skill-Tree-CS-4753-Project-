80fd25f59165f86080e1796119539d41afc87913
Faye Park <shp8xb@virginia.edu>
2018-11-17 18:50:35 -0500

    Login testing
    More login testing

--- sendlogin.php
+++ sendlogin.php
@@ -2,7 +2,7 @@
 require 'vendor/autoload.php';
 
 $email = $_POST["email"];
-$password = password_hash($_POST["password"], PASSWORD_BCRYPT, PASSWORD_DEFAULT);
+$password = $_POST["password"];
 $db_connection = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d9rsujpdd41171 user=zaimztdkhiptlg password=25fe2e242db912e7638b18668ef861180124a0081ad0bd07ca7f33e1bd6c7de8 sslmode=require");
 if ($db_connection->connect_error) {
     die("connection failed: " . $db_connection->connect_error);
@@ -10,7 +10,7 @@ if ($db_connection->connect_error) {
 
 echo "Hello1";
 
-$check = "SELECT * FROM \"siteUsers\" WHERE email='$email' and password='$password'";
+$check  = "SELECT * FROM table WHERE email='$email'";
 $result = pg_query($db_connection,$check);
 
 $url = "https://skill-tree-ecommerce-project.herokuapp.com/confirmation.html";
@@ -18,9 +18,14 @@ $url = "https://skill-tree-ecommerce-project.herokuapp.com/confirmation.html";
 echo "Hello2";
 
 if (pg_num_rows($result) == 1) {
-    echo "$email";
-    echo "$password";
-    echo "$result";
+    echo "We got email boys";
+
+    $row = mysqli_fetch_assoc($result);
+    $db_password = $row["id"];
+
+    if(password_verify($password, $db_password)) {
+        echo "Success!!!";
+    }
 }
 else {
     $url="https://skill-tree-ecommerce-project.herokuapp.com/login.php?login=0";
