9eb34e7866faa8c5e89cb3fe3a6762c4cffda20e
Faye Park <shp8xb@virginia.edu>
2018-11-17 17:59:53 -0500

    Added login capability
    Added login.php and sendlogin.php

---
+++ login.php
@@ -0,0 +1,103 @@
+<!DOCTYPE HTML>
+<!--
+	Twenty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Login - Skill Tree</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="contact is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+					<h1 id="logo"><a href="index.html">Skill Tree <span></span></a></h1>
+					<nav id="nav">
+						<ul>
+							<li class="current"><a href="index.html">Home</a></li>
+							<li class="current"><a href="browse.html">Browse</a></li>
+							<li class="current"><a href="aboutus.html">About Us</a></li>
+							<li class="current"><a href="contact.php">Contact Us</a></li>
+							<li class="current"><a href="login.html">Log In</a></li>
+							<li><a href="signup.php" class="button primary">Sign Up</a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Main -->
+				<article id="main">
+
+					<header class="special container">
+						<span class="icon fa-laptop"></span>
+						<h2>Welcome Back</h2>
+						<p>We are excited that you're here!</p>
+					</header>
+
+					<!-- One -->
+						<section class="wrapper style4 special container medium">
+
+							<!-- Content -->
+								<div class="content">
+									<form action = "sendlogin.php" method = "POST" name="loginform">
+										<div class="row gtr-50">
+											<?php if (isset ($_GET["login"]) && !$_GET["login"]): ?>
+													<div class="col-12">
+														<p class="erroremail" style="color: red;" id="erroremail">Invalid email/password combination.</p>
+													</div>
+											<?php endif; ?>
+											<div class="col-6 col-12-mobile">
+												<input type="text" name="email" placeholder="Email" />
+											</div>
+											<div class="col-6 col-12-mobile">
+												<input type="password" name="password" placeholder="Password" />
+											</div>
+											<div class="col-12 col-12-mobile">
+												<p><a href="#">Forgot Password?</a></p>
+											</div>
+											<div class="col-12">
+												<ul class="buttons">
+													<li><input type="submit" class="special" value="Login" /></li>
+												</ul>
+											</div>
+										</div>
+									</form>
+								</div>
+
+						</section>
+
+				</article>
+
+			<!-- Footer -->
+				<footer id="footer">
+
+					<ul class="icons">
+						<li><a href="https://github.com/Faye-Park/Skill-Tree-CS-4753-Project-" class="icon circle fa-github"><span class="label">Github</span></a></li>
+					</ul>
+
+					<ul class="copyright">
+						<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ sendlogin.php
@@ -0,0 +1,24 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+
+$email = $_POST["email"];
+$password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+$db_connection = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d9rsujpdd41171 user=zaimztdkhiptlg password=25fe2e242db912e7638b18668ef861180124a0081ad0bd07ca7f33e1bd6c7de8 sslmode=require");
+if ($db_connection->connect_error) {
+    die("connection failed: " . $db_connection->connect_error);
+}
+$check = "SELECT * FROM \"siteUsers\" WHERE email='$email' and password='$password'";
+$result = pg_query($db_connection,$check1;
+
+$url = "https://skill-tree-ecommerce-project.herokuapp.com/confirmation.html";
+
+if (pg_num_rows($result)) {
+    echo "Logged in";
+}
+else {
+    $url="https://skill-tree-ecommerce-project.herokuapp.com/loginj.php?login=0";
+}
+header("Location: $url");
+?>
