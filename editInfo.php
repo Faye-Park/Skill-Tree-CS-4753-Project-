55ceb0a837b5ec14ff5ac53d189995a5d7bb0b90
SarahD_UVA <sbd3ex@virginia.edu>
2018-11-19 19:59:06 -0500

    echo firstname

--- editInfo.php
+++ editInfo.php
@@ -1,4 +1,59 @@
-<form action = "saveinfo.php" method = "POST" name="editform">
+<!DOCTYPE HTML>
+<!--
+	Twenty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+
+<?php
+   session_start();
+?>
+
+<html>
+	<head>
+		<title>Signup - Skill Tree</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="contact is-preload">
+		<div id="page-wrapper">
+
+			<header id="header">
+				<h1 id="logo"><a href="index.php">Skill Tree<span></span></a></h1>
+				<nav id="nav">
+					<ul>
+						<li class="current"><a href="index.php">Home</a></li>
+						<li class="current"><a href="browse.php">Browse</a></li>
+						<li class="current"><a href="aboutus.php">About Us</a></li>
+						<li class="current"><a href="contact.php">Contact Us</a></li>
+						<?php if (isset($_SESSION['valid'])): ?>
+							<li class="current"><a href="logout.php">Log Out</a></li>
+							<li><a href="memberpage.php" class="button primary">Member Page</a></li>
+						<?php else: ?>
+							<li class="current"><a href="login.php">Log In</a></li>
+							<li><a href="signup.php" class="button primary">Sign Up</a></li>
+						<?php endif; ?>
+					</ul>
+				</nav>
+			</header>
+
+			<!-- Main -->
+				<article id="main">
+
+					<header class="special container">
+						<span class="icon fa-inbox"></span>
+						<h2>Start Learning Today</h2>
+						<p>Have an idea or passion you want to pursue? Begin today.</p>
+					</header>
+
+					<!-- One -->
+						<section class="wrapper style4 special container medium">
+
+							<!-- Content -->
+								<div class="content">
+									<form action = "sendinfo.php" method = "POST" name="editform">
 										<div class="row gtr-50">
 										<?php if (isset ($_GET["signup"]) && !$_GET["signup"]): ?>
 												<div class="col-12">
@@ -8,7 +63,7 @@
 
 											<div class="col-6 col-12-mobile">
 												First name
-												<input type="text" name="firstname" placeholder="First Name"/>
+												<input type="text" name="firstname" placeholder='<?php echo $firstname ?>' />
 											</div>
 											<div class="col-6 col-12-mobile">
 												Last name
@@ -102,3 +157,39 @@
 											</div>
 										</div>
 									</form>
+								</div>
+
+						</section>
+
+				</article>
+
+			<!-- Footer -->
+				<footer id="footer">
+
+					<ul class="icons">
+						<li><a href="https://github.com/Faye-Park/Skill-Tree-CS-4753-Project-" class="icon circle fa-github"><span class="label">Github</span></a></li>
+					</ul>
+
+					<ul class="copyright">
+						<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.validate.min.js"></script>
+			<script src="assets/js/validation.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
