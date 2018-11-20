376915f3375c6c509ed248dee3672653166dfca3
SarahD_UVA <sbd3ex@virginia.edu>
2018-11-19 21:35:11 -0500

    Update editInfo.php

--- editInfo.php
+++ editInfo.php
@@ -79,15 +79,15 @@ $lastname = $row['lastname'];
 
 											<div class="col-6 col-12-mobile">
 												First name
-												<input type="text" name="firstname" placeholder= <?php echo $firstname;?> />
+												<input type="text" name="firstname" placeholder="<?php echo $firstname;?>" />
 											</div>
 											<div class="col-6 col-12-mobile">
 												Last name
-												<input type="text" name="lastname" placeholder= <?php echo $lastname;?> />
+												<input type="text" name="lastname" placeholder="<?php echo $lastname;?>" />
 											</div>
 											<div class="col-12">
 												Email
-												<input type="text" name="email" placeholder= <?php echo $email;?> />
+												<input type="text" name="email" placeholder="<?php echo $email;?>" />
 											</div>
 											<div class="col-12">
 												Address
